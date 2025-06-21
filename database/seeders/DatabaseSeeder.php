81b67386a672c7ad074a897137c16b68ebb57279
Muhammad3a <118835652+Muhammad3a@users.noreply.github.com>
2025-06-21 18:19:12 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password'), // ini password-nya
+            'email' => 'admin@gmail.com',
+            'password' => 'adminbsi',
         ]);
     }
 }
